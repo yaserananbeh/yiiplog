467c2c7c62769bb1d44be0152a5b013d1daeb58b
yaserananbeh <yaser.ananbeh74@gmail.com>
2022-03-04 16:50:52 +0300

    first commit, add the migration files

---
+++ console/migrations/m220304_133804_create_post_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%post}}`.
+ */
+class m220304_133804_create_post_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%post}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(255),
+            'body' => 'LONGTEXT',
+            'created_at' => $this->integer(),
+            'updated_at' => $this->integer(),
+            'created_by' => $this->integer(),
+        ]);
+        $this->addForeignKey('FK_post_user_created_by', '{{%post}}', 'created_by', '{{%user}}', 'id');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('FK_post_user_created_by', '{{%post}}');
+        $this->dropTable('{{%post}}');
+    }
+}
---
+++ console/migrations/m220304_134212_create_comment_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%comment}}`.
+ */
+class m220304_134212_create_comment_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%comment}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(255),
+            'body' => $this->text(),
+            'post_id' => $this->integer(),
+            'created_at' => $this->integer(),
+            'updated_at' => $this->integer(),
+            'created_by' => $this->integer(),
+        ]);
+        $this->addForeignKey('FK_comment_user_created_by', '{{%comment}}', 'created_by', '{{%user}}', 'id');
+        $this->addForeignKey('FK_comment_post_post_id', '{{%comment}}', 'post_id', '{{%post}}', 'id');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('FK_comment_user_created_by', '{{%comment}}');
+        $this->dropForeignKey('FK_comment_post_post_id', '{{%comment}}');
+        $this->dropTable('{{%comment}}');
+    }
+}
