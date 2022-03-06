23c4aff2676f4e75e87e9b52ad3c41e1ac80efa2
yaserananbeh <yaser.ananbeh74@gmail.com>
2022-03-06 13:55:53 +0300

    implement the authenticator behaviors

--- frontend/controllers/CommentController.php
+++ frontend/controllers/CommentController.php
@@ -3,8 +3,19 @@
 namespace frontend\controllers;
 
 use frontend\resource\Comment;
+use yii\filters\auth\HttpBearerAuth;
 
 class CommentController extends \yii\rest\ActiveController
 {
     public $modelClass = Comment::class;
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator']['only'] = ['create', 'update', 'delete'];
+        $behaviors['authenticator']['authMethods'] = [
+            HttpBearerAuth::class
+        ];
+        return $behaviors;
+    }
 }
--- frontend/controllers/PostController.php
+++ frontend/controllers/PostController.php
@@ -3,8 +3,20 @@
 namespace frontend\controllers;
 
 use frontend\resource\Post;
+use yii\filters\auth\HttpBasicAuth;
+use yii\filters\auth\HttpBearerAuth;
 
 class PostController extends \yii\rest\ActiveController
 {
     public $modelClass = Post::class;
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator']['only'] = ['create', 'update', 'delete'];
+        $behaviors['authenticator']['authMethods'] = [
+            HttpBearerAuth::class
+        ];
+        return $behaviors;
+    }
 }
