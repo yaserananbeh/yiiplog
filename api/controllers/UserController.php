b071f2bee51a54a1fead9e79322964ff73e3e1b1
yaserananbeh <yaser.ananbeh74@gmail.com>
2022-03-07 21:55:41 +0200

    create the api file with user restful api

---
+++ api/controllers/UserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace api\controllers;
+
+use common\models\User;
+use yii\rest\ActiveController;
+
+class UserController extends ActiveController
+{
+    public $modelClass = User::class;
+}
