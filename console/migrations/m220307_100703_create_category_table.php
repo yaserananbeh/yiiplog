057a1a78f13c798ba59189740af48793f6f1a399
yaserananbeh <yaser.ananbeh74@gmail.com>
2022-03-07 13:41:27 +0200

    remove the category model edit eh migrations table

--- console/migrations/m220307_100703_create_category_table.php
+++ console/migrations/m220307_100703_create_category_table.php
@@ -15,7 +15,11 @@ class m220307_100703_create_category_table extends Migration
         $this->createTable('{{%category}}', [
             'id' => $this->primaryKey(),
             'name' => $this->string(),
+            'created_at' => $this->integer(),
+            'updated_at' => $this->integer(),
+            'created_by' => $this->integer(),
         ]);
+        $this->addForeignKey('FK_category_user_created_by', '{{%category}}', 'created_by', '{{%user}}', 'id');
     }
 
     /**
@@ -23,6 +27,7 @@ class m220307_100703_create_category_table extends Migration
      */
     public function safeDown()
     {
+        $this->dropForeignKey('FK_category_user_created_by', '{{%category}}');
         $this->dropTable('{{%category}}');
     }
 }
