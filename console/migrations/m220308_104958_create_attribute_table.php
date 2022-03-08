a79a6af460f799f4f78d4313ce04221b28fb95e2
yaserananbeh <yaser.ananbeh74@gmail.com>
2022-03-08 17:21:17 +0200

    edit the attribute table migration

--- console/migrations/m220308_104958_create_attribute_table.php
+++ console/migrations/m220308_104958_create_attribute_table.php
@@ -15,11 +15,13 @@ class m220308_104958_create_attribute_table extends Migration
         $this->createTable('{{%attribute}}', [
             'id' => $this->primaryKey(),
             'name' => $this->string(),
+            'category_id' => $this->integer(),
             'created_at' => $this->integer(),
             'updated_at' => $this->integer(),
             'created_by' => $this->integer(),
         ]);
         $this->addForeignKey('FK_attribute_user_created_by', '{{%attribute}}', 'created_by', '{{%user}}', 'id');
+        $this->addForeignKey('FK_attribute_category_category_id', '{{%attribute}}', 'category_id', '{{%category}}', 'id');
     }
 
     /**
@@ -27,6 +29,7 @@ class m220308_104958_create_attribute_table extends Migration
      */
     public function safeDown()
     {
+        $this->dropForeignKey('FK_attribute_category_category_id', '{{%attribute}}');
         $this->dropForeignKey('FK_attribute_user_created_by', '{{%attribute}}');
         $this->dropTable('{{%attribute}}');
     }
